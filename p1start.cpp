a4c2d0cb66b835135c1b8922d33a2bf31087c14a
mahinmy <649992025@qq.com>
2019-03-20 14:30:20 +0800

    class4basicFile

---
+++ p1start.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <class T>
+struct Node{
+    T data;
+    Node<T> *next;
+};
+
+template <class T>
+class LinkStack{
+private:
+    Node<T> *top;
+public:
+    LinkStack(){
+        top = NULL;
+    }
+    ~LinkStack(){
+        Node<T> *tmp = NULL;
+        while(top!=NULL){
+            tmp = top;
+            top = top->next;
+            delete tmp;
+        }
+    }
+    void PrintStack(){
+        Node<T> *tmp = NULL;
+        tmp = top;
+        cout << "Top of stack: ";
+        while(tmp!=NULL){
+            cout << tmp->data << " ";
+            tmp = tmp->next;
+        }
+        cout << "Bottom of stack" << endl;
+    }
+    void Push(T x){
+        Node<T> *s = NULL;
+        s = new Node<T>;
+        s->data = x;
+        s->next = top;
+        top = s;
+    }
+    T Pop(){
+        Node<T> *tmp = NULL;
+        if(top==NULL) throw "Empty";
+        tmp = top;
+        top = top->next;
+        T x = tmp->data;
+        delete tmp;
+        return x;
+    }
+    T GetTop(){
+        if(top!=NULL) return top->data;
+        else throw "Empty";
+    }
+    bool IsEmpty(){
+        return (top==NULL);
+    }
+};
+
+bool pBal(string S){
+    // TODO
+}
+
+int main(){
+    string s;
+    cin >> s;
+    cout << (pBal(s) ? "Balanced" : "Not balanced") << endl;
+    return 0;
+}
---
+++ p2start.cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <class T>
+struct Node{
+    T data;
+    Node<T> *next;
+};
+
+template <class T>
+class LinkList{
+private:
+    Node<T> *first, *last;
+    int length;
+public:
+    LinkList(){
+        first = new Node<T>;
+        first->next = NULL;
+        last = first;
+        length = 0;
+    }
+
+    LinkList(T a[], int n){ //tail insertion style
+        Node<T>* r, *s;
+        first = new Node<T>;
+        r=first;
+        for(int i=0;i<n;i++){
+            s = new Node<T>;
+            s->data = a[i];
+            r->next = s;
+            r = s;
+        }
+        r->next = NULL;
+        last = r;
+        length = n;
+    }
+
+    ~LinkList(){
+        Node<T> *q = NULL;
+        while(first!=NULL){
+            q=first;
+            first=first->next;
+            delete q;
+        }
+    }
+
+    void Insert(int i, T x){
+        Node<T> *p = first, *s = NULL;
+        int count = -1;
+        if(i<0) throw "Bad index";
+        while(p!=NULL && count < i-1){ //seek (i-1)-th point
+            p = p-> next;
+            count++;
+        }
+        if(p==NULL) throw "Bad index";
+        else{
+            s = new Node<T>;
+            s->data = x;
+            s->next = p->next;
+            p->next = s;
+            if(s->next==NULL) last=s;
+        }
+        length++;
+    }
+
+    void InsertLast(T x){
+        Node<T> *s = NULL;
+        s = new Node<T>;
+        s->data = x;
+        s->next = NULL;
+        last->next = s;
+        last = s;
+        length++;
+    }
+
+    T Delete(int i){
+        Node<T> *p = first, *q = NULL;
+        T x;
+        int count=-1;
+        if(i<0) throw "Bad index";
+        while(p!=NULL && count < i-1){ //seek (i-1)-th point
+            p = p-> next;
+            count++;
+        }
+        if(p==NULL || p->next == NULL) throw "Bad index";
+        else{
+            q=p->next;
+            x=q->data;
+            p->next = q->next;
+            if(last==q) last=p;
+            delete q;
+            length--;
+            return x;
+        }
+    }
+
+    int Locate(T x){
+        Node<T> *p = first->next;
+        int count=0;
+        while(p!=NULL){
+            if(p->data == x){
+                return count;
+            }
+            p = p->next;
+            count++;
+        }
+        return -1;
+    }
+
+    void printList(){
+        //print all the elements in this list, but will not give an "endl" at the end.
+        Node<T> *p = first->next;
+        while(p!=NULL){
+            cout << p->data << " ";
+            p = p->next;
+        }
+    }
+
+    T Get(int i){
+        Node<T> *p = first;
+        int count=-1;
+        if(i<0) throw "Bad index";
+        while(p!=NULL && count < i){ //seek i-th point
+            p = p-> next;
+            count++;
+        }
+        if(p==NULL) throw "Bad index";
+        return p->data;
+    }
+
+    int Length(){
+        return length;
+    }
+};
+
+template <class T>
+class LinkQueue{
+    private:
+        LinkList<T> q;
+    public:
+        LinkQueue(){
+            
+        }
+        ~LinkQueue(){
+              
+        }
+        void EnQueue(T x){
+            q.InsertLast(x);
+        }
+        T DeQueue(){
+            if(q.Length()==0) throw "Queue Empty";
+            return q.Delete(0);
+        }
+        T PeekQueue(){
+            if(q.Length()==0) throw "Queue Empty";
+            return q.Get(0);
+        }
+        bool IsEmpty(){
+            return (q.Length()==0);
+        }
+        void PrintQueue(){
+            cout << "Front of Queue: ";
+            q.printList();
+            cout << "End of Queue" << endl;
+        }
+};
+
+/* 
+    priority queue with at most 10 different levels of priority
+    Priority count starts from 0 (highest priority), 1 (next highest), etc...
+*/
+
+template <class T>
+class SimplePriorityQueue{
+    private:
+        
+    public:
+        SimplePriorityQueue(){
+            
+        }
+        SimplePriorityQueue(int numP){
+            
+        }
+        ~SimplePriorityQueue(){
+            
+        }
+        void EnQueue(T x, int p){ //the int here is for priority
+            
+        }
+        T DeQueue(){
+            
+        }
+        T PeekQueue(){
+            
+        }
+        bool IsEmpty(){
+            
+        }
+};
+
+int main(){
+    // TODO
+    SimplePriorityQueue<char> spq(6);
+    cout << "Before insertion, queue is " << (spq.IsEmpty()?"empty":"non-empty") << endl;
+    int priors[17]={0,1,2,3,4,5,0,3,2,4,0,2,1,5,1,3,2};
+    for(int i=0;i<17;i++){
+        spq.EnQueue('a'+i,priors[i]);
+    }
+    
+    cout << "Front of queue is now " << spq.PeekQueue() << endl;
+    // Push out first 10 items
+    for(int j=0;j<10;j++){
+        cout << spq.DeQueue() << " has left the queue" << endl;
+    }
+    cout << "Right now, queue is " << (spq.IsEmpty()?"empty":"non-empty") << endl;
+    cout << "Inserting some digits according to squares mod 6" << endl;
+    for(int i=0;i<10;i++){
+        spq.EnQueue('0'+i,i*i%6);
+    }
+    // Empty the queues
+    while(!spq.IsEmpty()){
+        cout << spq.DeQueue() << " has left the queue" << endl;
+    }
+    cout << "Right now, queue is " << (spq.IsEmpty()?"empty":"non-empty") << endl;
+    for(int i=0;i<10;i++){
+        spq.EnQueue('0'+i,i%6);
+    }
+    cout << "Right now, queue is " << (spq.IsEmpty()?"empty":"non-empty") << endl;
+    return 0;
+}
